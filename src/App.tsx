53230aa99b160c2f59f6dfe4f319b290eadb6243
rudivanek <rfv@datago.net>
2026-06-25 10:49:57 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ export default function App() {
       Sharpen.Studio
     </span>
     <span className="text-[7px] tracking-wide align-super">
-      V 1.1
+         V 1.1
     </span>
   </span>
 </span>
