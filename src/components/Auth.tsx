36d556be987cf2ee2a6586d3b781573c73bb236c
rudivanek <rfv@datago.net>
2026-06-25 11:01:22 -0600

    Updated Auth.tsx

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -38,8 +38,7 @@ export default function Auth() {
           <div className="w-10 h-10 bg-primary-600 flex items-center justify-center">
             <CheckSquare className="w-5 h-5 text-white" />
           </div>
-          <h1 className="text-2xl font-semibold text-slate-900">Task Flow - Sharpen.Studio
-  </h1>
+          <h1 className="text-2xl font-semibold text-slate-900">Task Flow - Sharpen.Studio </h1>
 
        
   </span>
